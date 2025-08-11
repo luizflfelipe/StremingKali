7bfcddfe87cf32ac88900637b8fc2d3818c1e5a0
Luiz Felipe <luisffelipe20@gmail.com>
2025-08-11 10:58:48 -0300

    Initial commit

---
+++ src/Filmes.java
@@ -0,0 +1,22 @@
+public class Filmes {               //Objeto na memoria (filme)
+    String nome;                       //Parametros que o filme possui
+    int anoDeLancamento;
+    int duracaoEmMinutos;
+    int disponivelNoPlano;
+    double somaDasAvaliacoes;
+    int totalAvaliacoes;
+
+    void exibeFichaTecnica() { //Metodo (void usado somente para registrar na memoria)
+        System.out.println("Nome do filme " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {    //Pode ser usado como a função,porem está dentro de uma classe entao é um metodo
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    double obtemMedia() {
+        return somaDasAvaliacoes/totalAvaliacoes;
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filmes meuFilme = new Filmes();
+        meuFilme.nome = "Kung Fu Panda";
+        meuFilme.duracaoEmMinutos = 140;
+        meuFilme.anoDeLancamento = 2012;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(7);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println(meuFilme.obtemMedia());
+     }
+}
